9cc6827743b8ac1bb587ee391d0e90c72a466049
Patrick-Rosenbaum <patrick.rosenbaum@gmx.de>
2022-08-31 18:14:10 +0200

    framework testing

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
-
-const express = require("express")
-const cors = require("cors")
-const csv = require("csv-parser")
-const fs = require("fs")
-const ObjectToCsv = require("objects-to-csv")
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
