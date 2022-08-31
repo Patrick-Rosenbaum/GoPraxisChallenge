8762306aa68707e2895e15f41725d11d733fa3d1
Patrick-Rosenbaum <patrick.rosenbaum@gmx.de>
2022-08-31 20:42:38 +0200

    some code tests

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	csvFile, err := os.Open("data.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened CSV file")
+	defer csvFile.Close()
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":5000") // listen and serve on 0.0.0.0:8080
+
+	/*r.POST("/todos", func(c *gin.Context) {
+		lastId := 0
+		for i := 0; i < len(todoListe); i++ {
+			currentId := parseInt(todoListe[i]["id"])
+
+			if currentId > lastId {
+				lastId = currentId
+			}
+		}
+	})*/
+
+	rGroup := r.Group("/todos")
+	rGroup.GET("")
+	rGroup.POST("")
+	rGroup.PUT("")
+	rGroup.DELETE("")
 }
