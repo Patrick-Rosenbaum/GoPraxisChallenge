53e29f867b5c7728104807b7e4aeadf3fec0463d
Patrick-Rosenbaum <patrick.rosenbaum@gmx.de>
2022-09-05 11:51:22 +0200

    more testing

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gofiber/fiber"
 )
 
+type list struct {
+	id   int64
+	name string
+	done bool
+}
+
+var todoListe = []list{}
+
+func getList(c *fiber.Ctx) error {
+	//loadCSV()
+
+	//todoList := strconv.Atoi(todoListe)
+	return c.JSON("data.json")
+}
+
+func addTodo(c *fiber.Ctx) error {
+	return c.JSON(todoListe)
+}
+
+func updateList(c *fiber.Ctx) error {
+	return c.JSON(todoListe)
+}
+
+func deleteTodo(c *fiber.Ctx) error {
+	return c.JSON(todoListe)
+}
 func main() {
-	fmt.Print()
+	app := fiber.New()
+
+	app.Get("/todos", getList)
+	app.Post("/todos", addTodo)
+	app.Put("/todos", updateList)
+	app.Delete("/todos/:id", deleteTodo)
+
+	app.Listen(":5000")
 }
