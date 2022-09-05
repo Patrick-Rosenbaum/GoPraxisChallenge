91d3c5f52aed1d87042fe4c9ec8f21b66743d68f
Patrick-Rosenbaum <patrick.rosenbaum@gmx.de>
2022-09-05 18:32:40 +0200

    added Get/Post not working

--- main.go
+++ main.go
@@ -1,34 +1,96 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 type list struct {
-	id   int64
-	name string
-	done bool
+	// uppercase sensitive
+	Id   string
+	Name string
+	Done bool
 }
 
 var todoListe = []list{}
 
 func getList(c *fiber.Ctx) error {
-	//loadCSV()
+	loadCSV()
 
-	//todoList := strconv.Atoi(todoListe)
-	return c.SendFile("data.json")
+	return c.JSON(todoListe)
 }
 
 func addTodo(c *fiber.Ctx) error {
-	return c.SendFile("data.json")
+
+	newEntry := new(list)
+	err := c.BodyParser((newEntry))
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Review your input", "data": err})
+	}
+
+	//max := 0
+	for i := 0; i < len(todoListe); i++ {
+		currId := todoListe[i]
+		fmt.Println(currId)
+		/*currId = strconv.Atoi(todoListe[i]["Id"])
+		if currId > max {
+			max = currId
+		}*/
+	}
+
+	return c.JSON(todoListe)
 }
 
 func updateList(c *fiber.Ctx) error {
-	return c.SendFile("data.json")
+
+	var id = c.BodyParser("Id")
+
+	for i := 0; i < len(todoListe); i++ {
+		if todoListe[i]["Id"] == id {
+			todoListe[i]["Done"] = c.BodyParser()["Done"]
+		}
+	}
+
+	return c.JSON(todoListe)
 }
 
 func deleteTodo(c *fiber.Ctx) error {
-	return c.SendFile("data.json")
+
+	id := c.Params("Id")
+
+	for i := 0; i < len(todoListe); i++ {
+		if todoListe[i]["Id"] == id {
+
+		}
+	}
+
+	return c.JSON(todoListe)
+}
+
+func loadCSV() {
+	var newList []list
+
+	file, err := os.Open("data.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	reader := csv.NewReader(file)
+	record, _ := reader.ReadAll()
+
+	for i := 1; i < len(record); i++ {
+		done := false
+		if record[i][2] == "true" {
+			done = true
+		}
+
+		readList := list{Id: record[i][0], Name: record[i][1], Done: done}
+		newList = append(newList, readList)
+	}
+	todoListe = newList
 }
 
 func main() {
